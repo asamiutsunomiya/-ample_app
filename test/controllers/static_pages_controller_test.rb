1f7826ee03c71c7270476b5eca85f85a4189ec0a
Asami <a_utsunomiya@webcircle.jp>
2019-07-04 14:57:50 +0700

    Add root_url test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+  end
+
 end
